fb71d5edc723bdb2643ae99070c0f03834887ead
Dev0 <dev0@syspulse.io>
2024-02-28 13:46:03 +0200

    Update Blockchain object (same as in Walle)

--- trunk-core/src/main/scala/io/syspulse/haas/core/Blockchain.scala
+++ trunk-core/src/main/scala/io/syspulse/haas/core/Blockchain.scala
@@ -4,15 +4,24 @@ import scala.jdk.CollectionConverters._
 import io.syspulse.skel.Ingestable
 
 case class Blockchain(
-  id:Blockchain.ID,
-  cid:Option[String] = None  // chain_id 
-)
+  name:String,
+  id:Option[String] = None  // chain_id 
+) {
+  def asLong:Long = id.getOrElse("0").toLong
+}
 
 object Blockchain {
   type ID = String
 
-  val ETHEREUM_MAINNET:ID = "ethereum"
-  val ZKSYNC_MAINNET:ID = "zksync"
-  val OPTIMISM_MAINNET:ID = "optimism"
-  val ARBITRUM_MAINNET:ID = "arbitrum"
+  val ETHEREUM = Blockchain("ethereum",Some("1"))
+  val BSC_MAINNET = Blockchain("bsc",Some("324"))
+  val ARBITRUM_MAINNET = Blockchain("arbitrum",Some("42161"))
+  val OPTIMISM_MAINNET = Blockchain("optimism",Some("10"))
+  val POLYGON_MAINNET = Blockchain("polygon",Some("137"))
+
+  val SCROLL_MAINNET = Blockchain("scroll",Some("534352"))
+  val ZKSYNC_MAINNET = Blockchain("zksync",Some("324"))
+  
+  val SEPOLIA = Blockchain("sepolia",Some("11155111"))
+  val ANVIL = Blockchain("anvil",Some("31337"))
 }
