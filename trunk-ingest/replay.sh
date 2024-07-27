f7bf49bc69e8965b3e4366774bb12bdf62808ba6
Dev0 <dev0@syspulse.io>
2024-07-27 17:03:33 +0300

    Add replay
    [+] take Extractor throttling

---
+++ trunk-ingest/replay.sh
@@ -0,0 +1,9 @@
+BLOCK_FILE=${1}
+TX_FILE=${2}
+
+./run-trunk.sh -e tx.extractor \
+   -f http://geth:8545 \
+   --block=list://${BLOCK_FILE} \
+   --filter=file://${TX_FILE} \
+   --batch=1 --block.throttle=1000 \
+   $@
